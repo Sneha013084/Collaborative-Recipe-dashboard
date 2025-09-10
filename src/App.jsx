bd1b750fb320e138621e145d2f0ed971728408d0
ahmadinasabemranparsa80-ui <ahmadinasabemranparsa80@gmail.com>
2025-09-10 18:35:07 +0000

    Setup App.jsx with Routes for RecipeList and RecipeDetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { useRecipes } from "./hooks/useRecipes";
 import RecipeList from "./components/RecipeList";
 import SearchBar from "./components/SearchBar";
+import RecipeDetailsPage from "./components/RecipeDetailsPage";
 import './App.css';
 
 /**
@@ -16,8 +18,18 @@ function App() {
   return (
     <div className="app">
       <h1>Recipe Finder</h1>
-      <SearchBar search={search} setSearch={setSearch} />
-      <RecipeList recipes={recipes} />
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <>
+              <SearchBar search={search} setSearch={setSearch} />
+              <RecipeList recipes={recipes} />
+            </>
+          }
+        />
+        <Route path="/recipe/:id" element={<RecipeDetailsPage />} />
+      </Routes>
     </div>
   );
 };
